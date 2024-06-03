1088938c45cec51edaca83ffadf2816081104efe
cyberorzech <natan.orze@gmail.com>
2024-06-03 19:30:12 +0200

    Implement BBDE

--- bbde.py
+++ bbde.py
@@ -4,6 +4,7 @@ from scipy.stats import truncnorm
 from tqdm import tqdm
 from math import exp
 
+from src.gauss import sample_multivariate_truncated_gaussian
 
 def initialize_population(population_size, bounds):
     dimensions = len(bounds)
@@ -24,10 +25,11 @@ def bbde(population, objective_func, iterations=100, alternative_exp_offset = Tr
     best_index = np.argmin(fitness)
     best_individual = population[best_index]
     best_score = objective_func(best_individual)
+    F = 0.8
     results = []
 
     # for i in tqdm(range(iterations), leave=False, desc=f'BBDE alt - {alternative_exp_offset}'):
-    for _ in tqdm(range(iterations)): # i to?
+    for _ in range(iterations): # i to?
         for individual in range(len(population)):
             # WYBIERA DWOCH LOSOWYCH OSOBNIKOW
             available_population_indexes = [idx for idx in range(popsize)]  # r1 != r2
@@ -35,12 +37,27 @@ def bbde(population, objective_func, iterations=100, alternative_exp_offset = Tr
 
             # TUTAJ ROZNE WARIANTY TEGO MUTANTA
             # TODO zwykly DE
-
+            # indices = np.random.choice(popsize, 3, replace=False)
+            # while individual in indices:
+            #     indices = np.random.choice(popsize, 3, replace=False)
+            # r1, r2, r3 = population[indices]
+            # mutant_classic = r1 + F * (r2 - r3)
+            
             # TODO zwykly BBDE
-            mutant = truncnorm.rvs(
-                (best_individual + individual) / 2,
-                
-            )
+            # HERE
+            #direction = r1 - r2
+            sigma = abs(best_individual - individual)
+            # sigma = np.std(direction)  # Standard deviation of the difference for Gaussian noise scaling
+            
+            #mutant_bbde = best_individual + np.random.normal(0, sigma, size=best_individual.shape)
+            midpoint = (best_individual + individual) / 2
+            mutant_bbde = np.random.normal(midpoint, sigma)  
+
+            #if random.uniform(0, 1) < 0.5:
+            #    mutant = mutant_classic
+            #else:
+            #    mutant = mutant_bbde                    
+
             # TODO z MSR
 
             # TODO z TPA
@@ -50,7 +67,8 @@ def bbde(population, objective_func, iterations=100, alternative_exp_offset = Tr
             # mutant = population[j] + exp(rand - exp_offset) * (r1 - r2)     # (2) local selection, exp(od -0.5 do 0.5), TUTAJ CHYBA WCHODZI MSR I TPA
             # calkowicie nowy punkt, stworzony wg powyzszej reguly
 
-            mutant = r1 # dummy, delete later
+            mutant = mutant_bbde# dummy, delete later
+            
             mutant_result = objective_func(mutant)
             # jesli wartosc funkcji celu dla mutanta jest lepsza (mniejsza) niz aktualnie rozpatrywany punkt: podmiana punktow i aktualizacja wartosci w fitness 
             # dodatkowo jesli jest najlepszy jak dotad to best index tez aktualizowany
@@ -73,7 +91,11 @@ def main():
 
     normalized_population, denorm_population = initialize_population(POPULATION_SIZE, BOUNDS)
     results = bbde(denorm_population, OBJECTIVE_FUNCTION)
-    print(results)
+    # evaluation
+    individuals = [result[0] for result in results]
+    scores = [result[1] for result in results]
+    
+    print(f"{sum(scores) / len(scores)}")
     
     
 
---
+++ eval-poc.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the Sphere function
+def sphere_function(x):
+    return np.sum(x**2, axis=1)
+
+# Generate random initial population
+population_size = 100
+dimensions = 10
+population = np.random.randn(population_size, dimensions)
+
+# Placeholder for BBDE implementation
+# Here you would integrate the BBDE algorithm
+# For now, let's assume we just evaluate the initial population
+fitness = sphere_function(population)
+
+# Plotting the fitness of the initial population
+plt.figure(figsize=(10, 5))
+plt.hist(fitness, bins=20, alpha=0.75)
+plt.title('Initial Fitness Distribution of Population')
+plt.xlabel('Fitness')
+plt.ylabel('Frequency')
+plt.show()
